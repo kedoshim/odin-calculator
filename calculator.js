faf2bfa79a3ce51615a71e3450e8af43244a1d4d
kedoshim <abraaodpaula@gmail.com>
2024-04-02 20:53:44 -0300

    add variables representing all buttons

---
+++ calculator.js
@@ -0,0 +1,28 @@
+const visor = document.querySelector("#visor");
+
+//Operators
+const add = document.querySelector("#add");
+const subtract = document.querySelector("#subtract");
+const divide = document.querySelector("#divide");
+const multiply = document.querySelector("#multiply");
+
+//Utilities
+const CE = document.querySelector("#CE");
+const backspace = document.querySelector("#backspace");
+const equals = document.querySelector("#equals");
+const dot = document.querySelector("#dot");
+
+//Numbers
+const numbers = [];
+
+numbers[0] = document.querySelector("#0");
+numbers[1] = document.querySelector("#1");
+numbers[2] = document.querySelector("#2");
+numbers[3] = document.querySelector("#3");
+numbers[4] = document.querySelector("#4");
+numbers[5] = document.querySelector("#5");
+numbers[6] = document.querySelector("#6");
+numbers[7] = document.querySelector("#7");
+numbers[8] = document.querySelector("#8");
+numbers[9] = document.querySelector("#9");
+
